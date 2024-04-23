432019fc0401936cf085d1cd9a510e96baa73d68
Дмитрий <kunicind1996@gmail.com>
2024-04-23 12:27:24 +0700

    add new logic with array for Laba 2

---
+++ Laba2/Product.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2
+{
+    internal class Product
+    {
+        private string _name;//название продукта
+        private string _description;// описание продукта
+        private double _price;// цена продукта
+        private int _quantity;// колличество продуктов
+        private string[] _structure;// состав продукта
+        private int _lenght_structure; //Максильное колличество продуктов на полках
+
+        public Product()
+        {// Конструктор без параметров
+            _name = "Defoult";
+            _description = string.Empty;
+            _price = double.NaN;
+            _quantity = 0;
+            _structure = Array.Empty<string>();
+        }
+
+        public Product(string name, string description, double price,
+            int quantity, int LenghtStructure)
+        {// Конструктор с параметрами
+            _name = name;
+            _description = description;
+            _price = price;
+            _quantity = quantity;
+            _structure = new string[LenghtStructure];
+        }
+
+        public string Name { get => _name; set => _name = value; }
+
+        public string Description { get => _description; set => _description = value; }
+        public double Price { get => _price; set => _price = value; }
+
+        public int Quantity { get => _quantity; set => _quantity = value; }
+
+        public string this[int index]
+        {
+            get { return _structure[index]; }
+            set { _structure[index] = value; }
+        }
+
+        public int LenghtStructure { get => _lenght_structure; set => _lenght_structure = value; }
+
+        public static int CounterProduct = 0;
+        public static int MaxProduct = 0;
+
+        /*Функция для увеличения счетчика продуктуктов*/
+        public static void IncreaseProductCount()
+        {
+            if (CounterProduct >= MaxProduct)
+            {
+                Console.WriteLine("Добавление дополнительных продуктов на полку невозможно");
+            }
+            else
+            {
+                CounterProduct++;
+
+            }
+        }
+
+        /*Функция для уменьшения счетчика продуктуктов*/
+        public static void DecreaseProductCount()
+        {
+            if (CounterProduct <= 0)
+            {
+                Console.WriteLine("Удалять больше нечего");
+            }
+            else { CounterProduct--; }
+        }
+
+        public static void AddStructProduct(Product product, string[] values)
+        {
+            if (values.Length == product._structure.Length)
+            {
+                for (int i = 0; i < values.Length; ++i)
+                {
+                    product[i] = values[i];
+                }
+            }
+            else
+            {
+                throw new Exception("Размер переданного массива не соответствует размеру массива структуры.");
+            }
+        }
+        public static Product AddProduct()
+        {
+            string name_prod = Print.GetInput("Введите название продукта");
+            string discripsion = Print.GetInput("Введите описание продукта");
+            double price_product = Print.GetDoubleInput("Введите цену продукта");
+            int quantity_product = Print.GetIntInput("Введите колличество товаров");
+            int lenght_struct = Print.GetIntInput("Сколько ингридиентов в составке продукта?");
+            string[] current_struct_product = new string[lenght_struct];
+            for (int i = 0; i < lenght_struct; ++i)
+            {
+                current_struct_product[i] = Print.GetInput($"введите ингридитент состава {i + 1} из {lenght_struct} :");
+            }
+            Product current_product = new Product(name_prod, discripsion, price_product, quantity_product, lenght_struct);
+            AddStructProduct(current_product, current_struct_product);
+
+            return current_product;
+        }
+
+        public static void RemoveProduct(Product[] allProducts)
+        {
+            string search_product_for_remove = Print.GetInput("Введите название продукта, который необходимо удалить");
+
+            Product productToRemove = Array.Find(allProducts, product =>
+            product != null && product.Name != null && product.Name.ToLower() == search_product_for_remove.ToLower());
+
+            if (productToRemove != null)
+            {
+                Array.Clear(allProducts, Array.IndexOf(allProducts, productToRemove), 1);
+                Console.WriteLine("Продукт успешно удален");
+            }
+            else
+            {
+                Console.WriteLine("Продукт не найден");
+            }
+        }
+
+        public static void PrintCurrentProduct(Product currentProduct)
+        {
+            if (currentProduct != null)
+            {
+                Console.WriteLine($"Название : " + currentProduct.Name);
+                Console.WriteLine($"Описание : " + currentProduct.Description);
+                Console.WriteLine($"Цена : " + currentProduct.Price);
+                Console.WriteLine($"Колличество на остатке : " + currentProduct.Quantity);
+                Console.WriteLine("Состав : ");
+                for (int i = 0; i < currentProduct._structure.Length; ++i)
+                {
+                    Console.Write($" {i + 1}. :" + currentProduct[i] + "\n");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Продукт не найден");
+            }
+        }
+
+        public static void PrintAllProduct(Product[] allProducts)
+        {
+            int count = 1;
+            if (allProducts != null)
+            {
+                foreach (Product product in allProducts)
+                {
+                    if (product == null)
+                    {
+                        continue;
+                    }
+                    Console.WriteLine($"Продукт #{count++} : ");
+                    Console.WriteLine($"Название : " + product.Name);
+                }
+            }
+            if (count == 1)
+            {
+                Console.WriteLine("Продуктов в магазине нету");
+            }
+        }
+
+        public static void SearchLinesInSelectedProduct(Product[] AllProducts, int selected)
+        {
+            bool IsNullAllProdOrNo = false;
+            switch (selected)
+            {
+                case 1://Название
+                    string name_search = Print.GetInput("Введите название по которому искать");
+                    bool name_check = false;
+                    for (int i = 0; i < AllProducts.Length; ++i)
+                    {
+                        if (AllProducts[i] == null)
+                        {
+                            continue;
+                        }
+                            IsNullAllProdOrNo = true;
+                        if (AllProducts[i].Name.ToLower() == name_search)
+                        {
+                            PrintCurrentProduct(AllProducts[i]);
+                            name_check = true;
+                        }
+                    }
+                    if (!name_check)
+                    {
+                        Console.WriteLine("Таких названий не найдено");
+                    }
+                    break;
+                case 2://Описание
+                    string dist_search = Print.GetInput("Введите строку по которой искать в описание");
+                    bool dist_check = false;
+                    for (int i = 0; i < AllProducts.Length; ++i)
+                    {
+                        if (AllProducts[i] == null)
+                        {
+                            continue;
+                        }
+                            IsNullAllProdOrNo = true;
+                        if (AllProducts[i].Description.ToLower().Contains(dist_search.ToLower()))
+                        {
+                            PrintCurrentProduct(AllProducts[i]);
+                            dist_check = true;
+                        }
+                    }
+                    if (!dist_check)
+                    {
+                        Console.WriteLine("Совпадений не найдено");
+                    }
+                    break;
+                case 3://Цена
+                    double price_search = Print.GetDoubleInput("Bведите цену по которой искать");
+                    bool price_check = false;
+                    for (int i = 0; i < AllProducts.Length; ++i)
+                    {
+                        if (AllProducts[i] == null)
+                        {
+                            continue;
+                        }
+                            IsNullAllProdOrNo = true;
+                        if (AllProducts[i].Price == price_search)
+                        {
+                            PrintCurrentProduct(AllProducts[i]);
+                            price_check = true;
+                        }
+                    }
+                    if (!price_check)
+                    {
+                        Console.WriteLine("Совпадений не найдено");
+                    }
+                    break;
+                case 4://Колличество
+                    int quantity_search = Print.GetIntInput("Введите колличество по которому искать совпадения");
+                    bool quantity_check = false;
+                    for (int i = 0; i < AllProducts.Length; ++i)
+                    {
+                        if (AllProducts[i] == null)
+                        {
+                            continue;
+                        }
+                            IsNullAllProdOrNo = true;
+                        if (AllProducts[i].Quantity == quantity_search)
+                        {
+                            PrintCurrentProduct(AllProducts[i]);
+                            quantity_check = true;
+                        }
+                    }
+                    if (!quantity_check)
+                    {
+                        Console.WriteLine("Совпадений не найдено");
+                    }
+                    break;
+                case 5://Состав
+                    string struct_search = Print.GetInput("введите состав по которому искать");
+                    bool struct_check = false;
+                    for (int i = 0; i < AllProducts.Length; ++i)
+                    {
+                        if (AllProducts[i] == null)
+                        {
+                            continue;
+                        }
+                            IsNullAllProdOrNo = true;
+                        for (int k = 0; k < AllProducts[i]._structure.Length; ++k)
+                        {
+
+                            if (AllProducts[i]._structure[k] == struct_search)
+                            {
+                                PrintCurrentProduct(AllProducts[i]);
+                                struct_check = true;
+                            }
+                        }
+                    }
+                    if (!struct_check)
+                    {
+                        Console.WriteLine("Совпадений не найдено");
+                    }
+                    break;
+
+            }
+            if(IsNullAllProdOrNo == false)
+            {
+                Console.WriteLine("Полки пусты");
+            }
+        }
+
+        public static void UpdateCurrentProduct(Product currentProduct, int selected)
+        {
+            switch (selected)
+            {
+                case 1:
+                    currentProduct.Description = Print.GetInput("Введите новое описание");
+                    break;
+                case 2:
+                    currentProduct.Price = Print.GetIntInput("Введите новую цену");
+                    break;
+                case 3:
+                    currentProduct.Quantity = Print.GetIntInput("Введите новое колличество продукта");
+                    break;
+                case 4:
+                    int newLenghtStruct = Print.GetIntInput("Сколько позиций будет в новом составе?");
+                    ResizeArrayProduct(ref currentProduct, newLenghtStruct);
+                    Console.WriteLine($"Введите новый состав на {newLenghtStruct} позиций");
+                    for (int i = 0; i < currentProduct._structure.Length; ++i)
+                    {
+                        currentProduct[i] = Print.GetInput($"{i + 1}. : ");
+                    }
+                    break;
+            }
+        }
+
+        public static bool CheckArrayIsNotNull(Product[] product)
+        {
+            bool check = false;
+            for (int i = 0; i < product.Length; ++i)
+            {
+                if (product[i] == null)
+                {
+                    check = true;
+                }
+            }
+            return check;
+        }
+
+        private static void ResizeArrayProduct(ref Product currentProduct, int newSize)
+        {
+            currentProduct._structure = new string[newSize];
+        }
+
+        public static Product SearchSelectedProduct(Product[] allProducts)
+        {
+            string current_name_product = Print.GetInput("Введите название продукта для его изменения");
+
+            Product productToRemove = Array.Find(allProducts, product =>
+            product != null && product.Name != null && product.Name.ToLower() == current_name_product.ToLower());
+            if (productToRemove != null)
+            {
+                return productToRemove;
+            }
+            return null;
+        }
+    }
+}
---
+++ Laba2/Program.cs
@@ -0,0 +1,89 @@
+﻿namespace Laba2
+//Product product1 = new Product("Магазин 1", "Продукт 1", "Описание продукта 1", 10.0, 20, 5);
+//Product product2 = new Product("Магазин 2", "Продукт 2", "Описание продукта 2", 15.0, 30, 3);
+//Product product3 = new Product("Магазин 3", "Продукт 3", "Описание продукта 3", 20.0, 25, 4);
+
+//Product.AddStructProduct(product1, new string[] { "Ингредиент 1", "Ингредиент 2", "Ингредиент 3", "Ингредиент 4", "Ингредиент 5" });
+//Product.AddStructProduct(product2, new string[] { "Ингредиент A", "Ингредиент B", "Ингредиент C" });
+//Product.AddStructProduct(product3, new string[] { "Ингредиент X", "Ингредиент Y", "Ингредиент Z", "Ингредиент W" });
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name_shop = Print.GetInput("Введите название вашего магазина");
+            Product.MaxProduct = Print.GetIntInput("Введите максимальное колличество продуктов на полках");
+            Product[] products = new Product[Product.MaxProduct];
+
+            string input_check = string.Empty;
+            while (input_check != "exit")
+            {
+                Console.Clear();
+                Print.PrintMenu(name_shop);
+                int choice = Print.GetIntInput("Выберите пункт меню");
+                Product curentProduct;
+                switch (choice)
+                {
+                    case 1:
+                        if (Product.CounterProduct < Product.MaxProduct)
+                        {
+                            products[Product.CounterProduct] = Product.AddProduct();
+                            Product.IncreaseProductCount();
+                            Console.WriteLine("Продукт успешно добавлен");
+                        }
+                        else
+                        {
+                            Console.WriteLine("На полках максимальное колличество продуктов");
+                        }
+                        break;
+                    case 2:
+                        if (Product.CounterProduct > 0)
+                        {
+                            Product.RemoveProduct(products);
+                            Product.DecreaseProductCount();
+                        }
+                        break;
+                    case 3:
+                        curentProduct = Product.SearchSelectedProduct(products);
+                        if (curentProduct != null)
+                        {
+                            Product.UpdateCurrentProduct(curentProduct, Print.PrintMenuForSelectedUpdateProduct());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Продукт не найден");
+                        }
+                        break;
+                    case 4:
+                        Product.SearchLinesInSelectedProduct(products, Print.PrintMenuForSearch());
+                        break;
+                    case 5:
+                        Product.PrintAllProduct(products);
+                        break;
+                    case 6:
+                        curentProduct = Product.SearchSelectedProduct(products);
+                        if (curentProduct != null)
+                        {
+                            Product.PrintCurrentProduct(curentProduct);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Продукт не найден");
+                        }
+                        break;
+                    case 7:
+                        input_check = "exit";
+                        break;
+
+                    default:
+                        Console.WriteLine("неверный пункт меню");
+                        break;
+                }
+                Print.WaitingForClick("Нажмите любую клавишу для продолжения");
+            }
+            Console.WriteLine("Завершение работы прораммы...");
+        }
+
+
+    }
+}
