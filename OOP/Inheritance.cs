835b3b766bafa42dfcb69a7dd5b8f30ecc0b7bad
Дмитрий <kunicind1996@gmail.com>
2024-05-07 16:48:05 +0700

    add lesson for inheritalance

---
+++ OOP/Inheritance.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Inheritance
+    {
+
+    }
+
+    public class Person
+    {
+        public readonly static int minAge = 1;
+        public const string typeName = "Person";
+        private string _name = " ";
+        int age = 1;
+        public virtual int Age
+        {
+            get => age;
+            set { if (value > 0 && value < 100) age = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+
+        public virtual void Print() => Console.WriteLine($"Person : {Name}");
+    }
+
+    class Employe : Person
+    {
+        // скрываем поля и константы базового класса
+        public new readonly static int minAge = 18;
+        public new const string typeName = "Employee";
+
+        public override int Age
+        {
+            get => base.Age;
+            set { if (value > 17 && value < 110) base.Age = value; }
+        }
+        public string Company { get; set; }
+        //В данном случае в классе Employee переопределено свойство Name.
+        //В блоке get нем мы берем значение свойства из базовового класса Person
+        //и присоединяем к нему "Mr./Ms.". В блоке set передаем полученное значение в реализацию свойства
+        //Name базового класса Person
+        public new string Name
+        {
+            get => $"Mr./Ms. {base.Name}";
+            set => base.Name = value;
+        }
+        public Employe(string name, string company) : base(name)
+        {
+            this.Company = company;
+            base.Age = 18;// возраст для работников по умолчанию
+        }
+        //Также можно запретить переопределение методов и свойств.
+        //В этом случае их надо объявлять с модификатором sealed
+        public override sealed void Print()
+        {
+            Console.WriteLine(Name + " работает в " + Company);
+            Console.WriteLine("ИЛИ");
+            base.Print();
+            Console.WriteLine(" работает в " + Company);
+        }
+    }
+
+    class Client : Person
+    {
+        public string Bank { get; set; }
+        public Client(string name, string bank) : base(name)
+        {
+            this.Bank = bank;
+        }
+        // Другим способом изменить функциональность метода, унаследованного от базового класса,
+        // является скрытие (shadowing / hiding).
+        public new void Print()
+        {//В каких ситуациях можно использовать скрытие?
+         //Например в родительском классе метод Print не является virtual
+            Console.WriteLine($"Работник {Name}, явлляется клиентом банка {Bank}");
+            //base.Print();   // вызываем метод Print из базового класса Person
+        }
+        //Фактически скрытие метода/свойства представляет определение в классе-наследнике метода или свойства,
+        //которые соответствует по имени и набору параметров методу или свойству базового класса.
+        //Для скрытия членов класса применяется ключевое слово new
+    }
+}
---
+++ OOP/Program.cs
@@ -0,0 +1,105 @@
+﻿namespace OOP
+{
+    internal class Program
+    {
+        /// <summary>принимает объект и тип, к которому нужно преобразовать этот объект. Она возвращает объект типа T или null, если преобразование не удалось</summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Наследование\n");
+            Person person = new Person("Dima");
+            person.Print();
+            person = new Employe("Serega", "Sber");
+            person.Print();
+
+
+            //Восходящие преобразования. Upcasting
+            Employe employe = new Employe("Tom", "Luch");
+
+            Person person1 = employe;// преобразование от Employee к Person
+
+            Console.WriteLine(person1.Name);
+
+            //Нисходящие преобразования. Downcasting
+
+            Employe employe1 = (Employe)person1; // преобразование от Person к Employee
+            Console.WriteLine(employe1.Company + " " + employe1.Name);
+
+            // Объект Employee также представляет тип object
+            object obj = new Employe("Bill", "Microsoft");
+
+            // чтобы обратиться к возможностям типа Employee, приводим объект к типу Employee
+            Employe employee = (Employe)obj;
+
+            Person person2 = new Client("Sam", "VTB");
+            person2.Print();
+            Client client = (Client)person2;
+            Console.WriteLine(client.Name + " " + client.Bank);
+
+            object obj2 = new Employe("Bill ", "Microsoft");
+
+            ((Employe)obj2).Print();
+
+            string company = ((Employe)obj2).Company;
+            Console.WriteLine(company);
+
+            //Способы преобразований
+            //ключевое слово as. С помощью него программа пытается преобразовать выражение
+            //к определенному типу, при этом не выбрасывает исключение
+
+            Person person3 = new Person("Boris");
+            Employe? employe2 = MyConvertTo<Employe>(person3);
+            if (employe2 == null) { Console.WriteLine("Преобразование прошло неудачно"); }
+            else { Console.WriteLine(employe2.Company); }
+
+            //Если значение слева от оператора представляет тип, указаный справа от оператора,
+            //то оператор is возвращает true, иначе возвращается false.
+            //Причем оператор is позволяет автоматически преобразовать значение к типу,
+            //если это значение представляет данный тип
+            Person jerry = new Person("Jerry");
+            if (jerry is Employe empl)
+            {
+                Console.WriteLine(empl.Company);
+            }
+            else
+            {
+                Console.WriteLine("Преобразование не допустимо");
+            }
+
+            //Виртуальные методы и свойства
+            //Те методы и свойства, которые мы хотим сделать доступными для переопределения,
+            //в базовом классе помечается модификатором virtual. Такие методы и свойства называют виртуальными
+            //А чтобы переопределить метод в классе-наследнике, этот метод определяется с модификатором override.
+            //Переопределенный метод в классе-наследнике должен иметь тот же набор параметров,
+            //что и виртуальный метод в базовом классе.
+
+            Employe tom = new Employe("Tom", "Microsoft");
+            // В классе Employee это свойство переопределено - возраст работника должен быть не меньше 18.
+            Console.WriteLine(tom.Age); // 18
+            tom.Age = 22;
+            Console.WriteLine(tom.Age); // 22
+            tom.Age = 12;
+            Console.WriteLine(tom.Age); // 22
+
+
+
+
+
+            Console.WriteLine("\n\n");
+
+
+        }
+        /// <summary>принимает объект и тип, к которому нужно преобразовать этот объект. Она возвращает объект типа T или null, если преобразование не удалось</summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj">The object.</param>
+        /// <returns>
+        ///   <br />
+        /// </returns>
+        public static T MyConvertTo<T>(object obj) where T : class
+        {
+            return obj as T;
+        }
+
+
+    }
+}
