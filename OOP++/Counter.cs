c8a24970d005db901774fef4058728f240202e0b
Дмитрий <kunicind1996@gmail.com>
2024-05-13 14:12:32 +0700

    lesson for operator in property

---
+++ OOP++/Counter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP__
+{
+    internal class Counter
+    {
+        public int Value { get; set; }
+
+        public static Counter operator +(Counter a, Counter b) // +
+        {
+            return new Counter { Value = a.Value + b.Value };
+        }
+        public static bool operator >(Counter a, Counter b) // > 
+        {
+            return a.Value > b.Value;
+        }
+        public static bool operator <(Counter a, Counter b) // < 
+        {
+            return a.Value < b.Value;
+        }
+
+        public static int operator +(Counter a, int b) // +
+        {
+            return a.Value + b;
+        }
+
+        public static Counter operator ++(Counter a) // ++
+        {
+            return new Counter { Value = a.Value + 10 };
+        }
+        public static bool operator !(Counter counter) // !
+        {
+            return counter.Value == 0;
+        }
+
+        public static bool operator true(Counter counter) // true
+        {
+            return counter.Value != 0;
+        }
+        public static bool operator false(Counter counter) // false
+        {
+            return counter.Value == 0;
+        }
+
+    }
+}
---
+++ OOP++/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace OOP__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter1 = new Counter { Value = 45};
+            Counter counter2 = new Counter { Value = 95};
+            bool result = counter1 > counter2;
+            Console.WriteLine(result);//false
+
+            Counter counter3 = counter1 + counter2;
+            Console.WriteLine(counter3.Value);//140
+
+            result = counter3 > counter2;
+            Console.WriteLine(result);//true
+
+            int resultInt = counter3 + 111;//251
+            Console.WriteLine(resultInt);
+
+            ++counter3;
+            Console.WriteLine(counter3.Value); // 150
+
+            counter3.Value += resultInt;
+
+            Console.WriteLine(counter3.Value);//401
+
+
+            Counter BoolConter = new Counter { Value = 0 };
+
+            if(!BoolConter) // BoolConter == 0
+            {
+                Console.WriteLine(true); // true
+            } else
+            {
+                Console.WriteLine(false);
+            }
+
+            if(BoolConter)
+            {
+                Console.WriteLine(true); 
+            }
+            else
+            {
+                Console.WriteLine(false); // false
+            }
+
+
+        }
+    }
+}
